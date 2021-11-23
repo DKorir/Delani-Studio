48de258fa427337130c8339ebe21cd09169293e7
DKorir <dominic.korir@student.moringaschool.com>
2021-11-23 14:22:43 +0300

    modify jQuery for responsive images

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+//jQuerry for what we do icon occlick navigation//
 $(document).ready(function(){
-    $("#img").on({
-        mouseover:function(){
-            $("#par").toggle()
-      },
-      mouseout:function(){
-            $("#par").toggle()
-      }
-    })
-    $("#img-1").on("click",function(){
-        $("#img-1").hide();
+    $("#img1").on("click",function(){
+        $("#img1").hide();
         $("#header4").addClass("header");
-        $("#para4").show();
+        $("#header4").css("text-align","center");
+        $("#header4").show();
         $("#para4").addClass("desc-para");
+        $("#para4").show();
     })
+    $("#img2").on("click",function(){
+        $("#img2").hide();
+        $("#header5").addClass("header");
+        $("#header5").show();
+        $("#para5").addClass("desc-para");
+        $("#para5").show();
+    })
+    $("#img3").on("click",function(){
+        $("#img3").hide();
+        $("#header6").addClass("header");
+        $("#header6").show();
+        $("#para6").addClass("desc-para");
+        $("#para6").show();
+    })
+    
 })
 
 //mouseonhover toggle portfolio images to show description//
@@ -43,3 +53,13 @@ $(document).ready(function(){
         $(".details").toggle()
     })
 });
+
+
+// message validation button//
+
+function myFunction(){
+    var myUsers=document.getElementById("user");
+    var myEmail=document.getElementById("email");
+    var myMessage=document.getElementById("msg");
+    alert("Hello " + user.value +" Thank you for your message " + "our support team will get back to you through " + myEmail.value);  
+}
